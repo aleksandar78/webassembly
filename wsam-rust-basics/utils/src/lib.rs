89091abc6797191bb4ff261b3fe6f73c70fb1f8b
Aleksandar Pavlovic <aleksandar.pavlovic78@gmail.com>
2018-10-21 23:42:28 +0200

    Use wasm bindgen for unsafe operations

--- wsam-rust-basics/utils/src/lib.rs
+++ wsam-rust-basics/utils/src/lib.rs
@@ -1,12 +1,20 @@
+#![feature(use_extern_macros)]
+
+extern crate wasm_bindgen;
+
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
 extern {
-    fn appendNumberToBody(x: u32);
-    fn alert(x: u32);
+//    fn appendNumberToBody(x: u32);
+    fn appendStringToBody(s: &str);
+//    fn alert(x: u32);
 }
 
-#[no_mangle]
+#[wasm_bindgen]
 pub extern fn run() {
-    unsafe {
-        appendNumberToBody(42);
-        alert(36);
-    }
+
+//        appendNumberToBody(42);
+//        alert(36);
+    appendStringToBody("Hi from Rust");
 }
